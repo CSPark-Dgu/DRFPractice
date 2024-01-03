29693b1bff68132cbfc8a3c4b310efe068084033
Changsu Park <gd12091@gmail.com>
2024-01-03 23:36:43 +0900

    migrate and basic APIs

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from .serializers import PostSerializer
+from .models import Post
 
-# Create your views here.
+
+class PostViewSet(ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+# def post_list(request):   #원래는 이런 식으로 구현해야 함
+#     pass
+#
+# def post_detial(request, pk):
+#     pass
