91654cf740f02d8e060d7c51e854ee41375ca9c6
Changsu Park <gd12091@gmail.com>
2024-01-03 23:55:14 +0900

    HTTP test

--- instagram/views.py
+++ instagram/views.py
@@ -7,6 +7,9 @@ class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
 
 # def post_list(request):   #원래는 이런 식으로 구현해야 함
 #     pass
