b9c15ebe52cccd0d41cb6b3eebea719bf79553b6
Changsu Park <gd12091@gmail.com>
2024-01-05 23:31:53 +0900

    JSON Serializer

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,5 +7,6 @@ router.register("post", views.PostViewSet)
 # router.urls #url pattern list
 
 urlpatterns = [
+    path("public/", views.PublicPostListAPIView.as_view()),
     path("", include(router.urls)),
 ]
