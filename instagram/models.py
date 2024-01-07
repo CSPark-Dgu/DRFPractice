d588d87495328e93466f5c5945c5486fd3d0f7a0
Changsu Park <gd12091@gmail.com>
2024-01-07 14:30:26 +0900

    Fix:model

--- instagram/models.py
+++ instagram/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class Post(models.Model):  # versus DRF
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     message = models.TextField()
+    tag_set = models.ManyToManyField("Tag", blank=True)
+    photo = models.ImageField(blank=True, upload_to="instagram/%Y/%m/%d")
+    is_public = models.BooleanField(default=False, verbose_name="공개여부")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    is_public = models.BooleanField(default=False, db_index=True)
